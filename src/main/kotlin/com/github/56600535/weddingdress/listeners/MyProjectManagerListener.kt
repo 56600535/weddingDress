1bfb20e83d140dc24819d9b769538b540b250c34
GitHub Action <action@github.com>
2021-12-22 12:48:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/56600535/weddingdress/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/56600535/weddingdress/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.56600535.weddingdress.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.56600535.weddingdress.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/56600535/weddingdress/services/MyProjectService.kt
+++ src/main/kotlin/com/github/56600535/weddingdress/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.56600535.weddingdress.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.56600535.weddingdress.MyBundle
 
 class MyProjectService(project: Project) {
 
